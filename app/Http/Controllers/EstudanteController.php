fd8daabdb7a0749eaa9939c89403928822719f58
herquiloidehele <herquiloide@gmail.com>
2017-10-03 11:22:39 +0200

    criacao de controlers e interface

---
+++ app/Http/Controllers/EstudanteController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Estudante;
+use App\Models\interfaces\InterfaceController;
+use App\Models\interfaces\lista;
+use Illuminate\Http\Request;
+
+class EstudanteController extends Controller implements InterfaceController
+{
+
+    /**
+     * funcao que busca lista de objectos
+     * @return lista de todos objectos
+     */
+    public function listar()
+    {
+        return response()->json(['estudantes' => Estudante::all()], 200);
+    }
+
+    /**
+     * Salvar um determinado Objecto
+     * @param Request $objecto - objecto a ser salvo
+     * @return $objecto - objecto se for salvo
+     */
+    public function salvar(Request $objecto)
+    {
+        $estudante = Estudante::create($objecto->all());
+        return response()->json(['estudante'=> $estudante], 200);
+    }
+
+    /**
+     * @param Request $objecto - O objecto a ser actualizado
+     * @param $id - a chave primaria do objecto
+     * @return $objecto se for actualizado
+     */
+    public function editar(Request $objecto, $id)
+    {
+        $estudante = Estudante::find($id);
+        if(!$estudante)
+            return response()->json(['mensagem' => 'Estudante nao encontrado'], 404);
+        else {
+            $estudante->update($objecto->all());
+            return response()->json(['estudante' => $estudante], 200);
+        }
+    }
+
+    /**
+     * @param $id - do objecto pesquisado
+     * @return $objecto encontrado
+     */
+    public function pesquisar($id)
+    {
+        $estudante = Estudante::find($id);
+        if(!$estudante)
+            return response()->json(['mensagem' => 'Estudante nao encontrado'], 404);
+        else
+            return response()->json(['mensagem' => $estudante]);
+    }
+
+    /**
+     * @param Request $objecto - O objecto a ser removido
+     * @param $id - a chave primaria do objecto
+     * @return $objecto se for removido
+     */
+    public function remover(Request $objecto, $id)
+    {
+
+    }
+
+    /**
+     * funcao que salva um conjunto de objectos numa transacao
+     * em um eh dependente do outro
+     * @param Request[] ...$objectos - conjunto de objectos a serem salvos
+     * @return $object - conjunto de objectos salvos
+     */
+    public function salvarTransacao(Request ...$objectos)
+    {
+
+    }
+
+    /**
+     * funcao que pesquisa baseado em varios atributos
+     * @param array ...$atributos - conjunto de atributos que serao usados para a pesquisa
+     * @return $objecto retornado
+     */
+    public function pesquisarMuitos(...$atributos)
+    {
+
+    }
+}
--- app/Models/interfaces/InterfaceController.php
+++ app/Models/interfaces/InterfaceController.php
@@ -1,6 +1,11 @@
 <?php
+
+namespace App\Models\interfaces;
+use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\Integer;
+
 /**
- * Created by PhpStorm.
+ * Esta interface eh generica para dos os controllers da aplicacao
  * User: herquiloidehele
  * Date: 10/1/17
  * Time: 17:52
@@ -9,10 +14,61 @@
 interface InterfaceController{
 
 
+    /**
+     * funcao que busca lista de objectos
+     * @return lista de todos objectos
+     */
+    public function listar();
 
 
+    /**
+     * Salvar um determinado Objecto
+     * @param Request $objecto - objecto a ser salvo
+     * @return $objecto - objecto se for salvo
+     */
+    public function salvar(Request $objecto);
 
-    public function save();
+
+    /**
+     * @param Request $objecto - O objecto a ser actualizado
+     * @param $id - a chave primaria do objecto
+     * @return $objecto se for actualizado
+     */
+    public function editar(Request $objecto, $id);
+
+
+    /**
+     * @param $id - do objecto pesquisado
+     * @return $objecto encontrado
+     */
+    public function pesquisar($id);
+
+
+    /**
+     * @param Request $objecto - O objecto a ser removido
+     * @param $id - a chave primaria do objecto
+     * @return $objecto se for removido
+     */
+    public function remover(Request $objecto, $id);
+
+
+    /**
+     * funcao que salva um conjunto de objectos numa transacao
+     * em um eh dependente do outro
+     * @param Request[] ...$objectos - conjunto de objectos a serem salvos
+     * @return $object - conjunto de objectos salvos
+     */
+    public function salvarTransacao(Request ...$objectos);
+
+
+    /**
+     * funcao que pesquisa baseado em varios atributos
+     * @param array ...$atributos - conjunto de atributos que serao usados para a pesquisa
+     * @return $objecto retornado
+     */
+    public function pesquisarMuitos(...$atributos);
+
+
 
 
 }
