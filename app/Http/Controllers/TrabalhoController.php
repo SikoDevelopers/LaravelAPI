b9fb3805d18d150dcc789ee242f21c3cd23fc5c2
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-18 18:55:21 +0200

    melhoramento da classe trabalhoController

--- app/Http/Controllers/TrabalhoController.php
+++ app/Http/Controllers/TrabalhoController.php
@@ -45,46 +45,46 @@ class TrabalhoController extends ModelController
 
     public function salvar(Request $objecto) {
 
-
-        $trabalhoPrincipal = new Trabalho();
-        //PegarEstudante
-        $estudante = new Estudante();
-
-        $estudante = Estudante::where('users_id',$objecto->user)->first();
-        $trabalhoPrincipal->estudantes_id=$estudante->id;
-        $trabalhoPrincipal->titulo=$objecto->titulo;
-        $trabalhoPrincipal->descricao=$objecto->descricao;
-        $trabalhoPrincipal->save();
-
-
-        //Gravacao de Supervisor
-        if($objecto->tipoSup==1){
-            $docenteAreaTra = new DocentesAreasTrabalho();
-            $docenteAreaTra->trabalhos_id =$trabalhoPrincipal->id;
-            $docenteAreaTra->funcoes_id = 1;
-            $docenteAreaTra->docente_areas_id = $this->pesquisarSupervisorArea($objecto->supervisor_id,$objecto->areas_id,$objecto->tipoSup)->id;
-            $docenteAreaTra->save();
-        }elseif ($objecto->tipoSup==2){
-
-            $trabalhoPrincipal->areas_supervisor_externos_id =$this->pesquisarSupervisorArea($objecto->supervisor_id,$objecto->areas_id,$objecto->tipoSup)->id;
-        }
-
-
-//        //Gravacao do protocolo
-        $ficheiro_protcolo = new FicheirosTrabalho();
-        Storage::putFileAs('public',$objecto->file('file'),'protocolo.pdf'.$objecto->user);
-        $ficheiro_protcolo->data= $objecto->data;
-        $ficheiro_protcolo->caminho='protocolo.pdf'.$objecto->user;
-        $ficheiro_protcolo->categorias_ficheiros_id =1;
-        $ficheiro_protcolo->trabalhos_id=$trabalhoPrincipal->id;
-        $ficheiro_protcolo->save();
-
-
-
-////            $sup = $trabalhoPrincipal->supervisor();
-
-        $trabalhoPrincipal->save();
-        return response()->json(['trabalho'=>Trabalho::find($trabalhoPrincipal->id)]);
+//
+//        $trabalhoPrincipal = new Trabalho();
+//        //PegarEstudante
+//        $estudante = new Estudante();
+//
+//        $estudante = Estudante::where('users_id',$objecto->user)->first();
+//        $trabalhoPrincipal->estudantes_id=$estudante->id;
+//        $trabalhoPrincipal->titulo=$objecto->titulo;
+//        $trabalhoPrincipal->descricao=$objecto->descricao;
+//        $trabalhoPrincipal->save();
+//
+//
+//        //Gravacao de Supervisor
+//        if($objecto->tipoSup==1){
+//            $docenteAreaTra = new DocentesAreasTrabalho();
+//            $docenteAreaTra->trabalhos_id =$trabalhoPrincipal->id;
+//            $docenteAreaTra->funcoes_id = 1;
+//            $docenteAreaTra->docente_areas_id = $this->pesquisarSupervisorArea($objecto->supervisor_id,$objecto->areas_id,$objecto->tipoSup)->id;
+//            $docenteAreaTra->save();
+//        }elseif ($objecto->tipoSup==2){
+//
+//            $trabalhoPrincipal->areas_supervisor_externos_id =$this->pesquisarSupervisorArea($objecto->supervisor_id,$objecto->areas_id,$objecto->tipoSup)->id;
+//        }
+//
+//
+////        //Gravacao do protocolo
+//        $ficheiro_protcolo = new FicheirosTrabalho();
+//        Storage::putFileAs('public',$objecto->file,'protocolo.pdf'.$objecto->user);
+//        $ficheiro_protcolo->data= $objecto->data;
+//        $ficheiro_protcolo->caminho='protocolo.pdf'.$objecto->user;
+//        $ficheiro_protcolo->categorias_ficheiros_id =1;
+//        $ficheiro_protcolo->trabalhos_id=$trabalhoPrincipal->id;
+//        $ficheiro_protcolo->save();
+//
+//
+//
+//////            $sup = $trabalhoPrincipal->supervisor();
+//
+//        $trabalhoPrincipal->save();
+        return response()->json(['trabalho'=>$objecto->titulo]);
 
 
     }
