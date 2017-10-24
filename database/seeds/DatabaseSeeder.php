0519753ed7f21bc7a3cbe9e0c8c7fc945e04d4a8
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-24 13:12:18 +0200

    corrigindo o factory2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         factory(\App\Models\Curso::class, 4)->create();
         factory(\App\Models\Docente::class,20)->create();
         factory(\App\Models\Funcao::class, 4)->create();
-//        factory(\App\Models\CategoriaFicheiro::class, 2)->create();
+        factory(\App\Models\CategoriaFicheiro::class, 2)->create();
         factory(\App\Models\EstadoFicheiro::class, 5)->create();
         factory(\App\Models\CategoriaEvento::class, 2)->create();
         factory(\App\Models\EstadoEvento::class, 3)->create();
@@ -28,8 +28,8 @@ class DatabaseSeeder extends Seeder
         factory(\App\Models\DirectorCurso::class, 4)->create();
         factory(\App\Models\DocenteArea::class,20)->create();
         factory(\App\Models\Tema::class,20)->create();
-        factory(\App\Models\Trabalho::class,20)->create();
-        factory(\App\Models\DocentesAreasTrabalho::class,20)->create();
+        factory(\App\Models\Trabalho::class,10)->create();
+        factory(\App\Models\DocentesAreasTrabalho::class,30)->create();
         factory(\App\Models\FicheiroReprovado::class,5)->create();
         factory(\App\Models\FicheirosTrabalho::class,20)->create();
         factory(\App\Models\FicheiroTrabalho_EstadoFicheiro::class,20)->create();
