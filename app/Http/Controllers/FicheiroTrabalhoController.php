68a614d9db21790bcb9990251df0856385f62640
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-25 15:06:55 +0200

    working with files 2

--- app/Http/Controllers/FicheiroTrabalhoController.php
+++ app/Http/Controllers/FicheiroTrabalhoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\FicheirosTrabalho;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class FicheiroTrabalhoController extends ModelController
 {
@@ -13,4 +15,14 @@ class FicheiroTrabalhoController extends ModelController
         $this->nomeObjectos = 'ficheiroTrabalhos';
         $this->relacionados = ['categoriaFicheiro','trabalho','estadoFicheiros','ficheiroReprovado'];
     }
+
+
+public function getFicheiros($id){
+          $ficheiros = DB::table('ficheiros_trabalhos')->where('trabalhos_id',id);
+          $allFiles=[];
+        foreach ($ficheiros as $index => $ficheiro){
+
+            $allFiles[$index] = Storage::url($ficheiro['caminho']);
+    }
+}
 }
