b90f1ef861ee3842ce32cf64e9abf32cc0f0a4c2
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-25 15:06:48 +0200

    working with files 1

--- app/Http/Controllers/EstudanteController.php
+++ app/Http/Controllers/EstudanteController.php
@@ -113,8 +113,11 @@ class EstudanteController extends ModelController {
     }
 
 
+    public function getEstduanteByuser($id){
+        $estudante = DB::table('estudantes')->where('users_id', $id);
 
-
+        return response()->json(['estudante'=>$estudante]);
+    }
 
 
 
